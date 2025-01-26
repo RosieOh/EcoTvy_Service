27c22f85f391e89efd0c538a889beefb165c8015
RosieOh <dhxogns920@gmail.com>
2025-01-26 21:06:28 +0900

    FEAT : Hero 컴포넌트 추가

---
+++ frontend/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <section className="hero is-primary is-medium">
+            <div className="hero-body">
+                <div className="container has-text-centered">
+                    <h1 className="title">Waste Management System</h1>
+                    <h2 className="subtitle">환경을 지키는 스마트 솔루션</h2>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
