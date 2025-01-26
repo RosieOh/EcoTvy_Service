dbc2f7cbc32ed34e35786e872fa7c2a17ed811e0
RosieOh <dhxogns920@gmail.com>
2025-01-26 21:06:22 +0900

    FEAT : Footer 컴포넌트 추가

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer has-background-dark">
+            <div className="content has-text-centered has-text-white">
+                <p>
+                    <strong>Waste Management</strong> by Your Company
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
