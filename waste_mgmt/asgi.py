736b53f8d5f79b01786a0a5dea7005d49ccd6640
RosieOh <dhxogns920@gmail.com>
2025-01-26 20:09:07 +0900

    FEAT : WebSocket 처리를 위한 ASGI 설정 추가

--- waste_mgmt/asgi.py
+++ waste_mgmt/asgi.py
@@ -1,16 +1,14 @@
-"""
-ASGI config for waste_mgmt project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from ocr.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waste_mgmt.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # HTTP 요청 처리
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)  # WebSocket URL 라우팅
+    ),
+})
