17e12493576f8cba9da8eaf559b68d1a6e3c1e15
RosieOh <dhxogns920@gmail.com>
2025-01-26 20:37:34 +0900

    CHORE : WebSocket 전체 라우팅을 위한 임포트 변경

--- waste_mgmt/asgi.py
+++ waste_mgmt/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from ocr.routing import websocket_urlpatterns
+from waste_mgmt.routing import websocket_urlpatterns  # 통합된 라우팅 패턴 가져오기
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'waste_mgmt.settings')
 
